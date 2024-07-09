0a26102df5d05c35921b7ac7fc2c326be3505de0
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-07-09 12:56:18 -0300

    app edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 export function App() {
-
   return (
-    <h1>Hello World</h1>
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6 text-center">
+        <p>Convide seus amigos e planeje sua próxima viagem!</p>
+        <div>
+
+        </div>
+        <p>Ao planejar sua viagem pela plann.er você automaticamente concorda
+        com nossos <a href="#" className="">termos de uso</a> e <a href="#" className="">políticas de privacidade</a>.</p>
+      </div>
+    </div>
   )
 }
 
